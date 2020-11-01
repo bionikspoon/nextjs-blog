076b90880a6acaece5621da57347d905f5ddede5
Manu Phatak <manu.phatak@procore.com>
2020-11-01 15:59:29 -0800

    create dynamic routes for posts

--- lib/posts.ts
+++ lib/posts.ts
@@ -10,20 +10,17 @@ export interface PostData {
   date: string
 }
 
+export async function getAllPostIds(): Promise<{ params: { id: string } }[]> {
+  const fileNames = await getPostFiles()
+
+  return fileNames.map(({ id }) => ({ params: { id } }))
+}
+
 export async function getSortedPostsData(): Promise<PostData[]> {
-  const fileNames = await fs.promises.readdir(postsDirectory)
+  const files = await getPostFiles()
 
   const allPostsData = await Promise.all(
-    fileNames.map(async (fileName) => {
-      const id = fileName.replace(/\.md$/, '')
-
-      const fullPath = path.join(postsDirectory, fileName)
-      const fileContents = await fs.promises.readFile(fullPath, 'utf-8')
-
-      const matterResult = matter(fileContents)
-      // TODO toPostData
-      return { id, ...matterResult.data } as PostData
-    })
+    files.map((file) => file.id).map(getPostData)
   )
 
   return allPostsData.sort((a, b) => {
@@ -33,4 +30,22 @@ export async function getSortedPostsData(): Promise<PostData[]> {
       return -1
     }
   })
+}
+
+export async function getPostData(id: string): Promise<PostData> {
+  const fullPath = path.join(postsDirectory, `${id}.md`)
+  const fileContents = await fs.promises.readFile(fullPath, 'utf-8')
+
+  const matterResult = matter(fileContents)
+  // TODO toPostData
+  return { id, ...matterResult.data } as PostData
+}
+
+async function getPostFiles() {
+  const fileNames = await fs.promises.readdir(postsDirectory)
+
+  return fileNames.map((fileName) => ({
+    fileName,
+    id: fileName.replace(/\.md$/, ''),
+  }))
 }
---
+++ pages/posts/[id].tsx
@@ -0,0 +1,54 @@
+import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from 'next'
+import Head from 'next/head'
+import Link from 'next/link'
+import { ParsedUrlQuery } from 'querystring'
+import { Layout } from '../../components/Layout'
+import { getAllPostIds, getPostData, PostData } from '../../lib/posts'
+
+interface QueryArgs extends ParsedUrlQuery {
+  id: string
+}
+interface PostStaticProps {
+  postData: PostData
+}
+
+interface PostProps extends InferGetStaticPropsType<typeof getStaticProps> {}
+
+export default function Post(props: PostProps): JSX.Element {
+  return (
+    <Layout>
+      <Head>
+        <title>{props.postData.title}</title>
+      </Head>
+      <h1>{props.postData.title}</h1>
+      <p>{props.postData.id}</p>
+      <p>{props.postData.date}</p>
+      <h2>
+        <Link href="/">back to home</Link>
+      </h2>
+    </Layout>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths<{ id: string }> = async () => {
+  return {
+    paths: await getAllPostIds(),
+    fallback: false,
+  }
+}
+
+export const getStaticProps: GetStaticProps<
+  PostStaticProps,
+  QueryArgs
+> = async (context) => {
+  if (context.params === undefined) {
+    // TODO: when is this undefined?
+    throw new Error(`context.params is undefined`)
+  }
+
+  return {
+    props: {
+      postData: await getPostData(context.params.id),
+    },
+  }
+}
